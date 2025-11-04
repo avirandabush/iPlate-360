da1e6331cccec67f48807a0d4872fa36d6ffa7af
Aviran Dabush <avirandabush@gmail.com>
2025-11-04 18:06:35 +0200

    featuring watermark logo for empty view.

--- plate360/Views/EmptyView/EmptyView.swift
+++ plate360/Views/EmptyView/EmptyView.swift
@@ -9,10 +9,19 @@ import UIKit
 
 class EmptyView: UIView {
     
+    private lazy var logoWatermarkImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "logo-circle")
+        imageView.contentMode = .scaleAspectFit
+        imageView.alpha = 0.1
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     private lazy var messageLabel: UILabel = {
         let label = UILabel()
         label.textColor = .white
-        label.font = .systemFont(ofSize: 17, weight: .medium)
+        label.font = .systemFont(ofSize: 22, weight: .medium)
         label.textAlignment = .center
         label.numberOfLines = 0
         label.text = "Search a Plate Number!"
@@ -32,9 +41,15 @@ class EmptyView: UIView {
     
     private func setup() {
         backgroundColor = .clear
+        addSubview(logoWatermarkImageView)
         addSubview(messageLabel)
         
         NSLayoutConstraint.activate([
+            logoWatermarkImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            logoWatermarkImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            logoWatermarkImageView.leadingAnchor.constraint(greaterThanOrEqualTo: leadingAnchor, constant: 16),
+            logoWatermarkImageView.trailingAnchor.constraint(lessThanOrEqualTo: trailingAnchor, constant: -16),
+            
             messageLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
             messageLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
             messageLabel.leadingAnchor.constraint(greaterThanOrEqualTo: leadingAnchor, constant: 16),
